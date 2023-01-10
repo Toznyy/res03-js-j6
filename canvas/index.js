75c79030c438b239a2c395ee9bdc7a13fca5a662
Toznyy <anthony.cormier29860@gmail.com>
2023-01-10 16:20:10 +0000

    ajout de la fin

--- canvas/index.js
+++ canvas/index.js
@@ -32,28 +32,50 @@ function exercice3() {
 }
 
 function exercice4() {
-
+    
+    let canvasDom = document.querySelector('#ex4');
+    let ctx = canvasDom.getContext('2d');
     let circle = {
         color: "#0000FF",
         radius: 100,
-        x: 120,
-        y: 130
+        firstx: 100,
+        x: 100,
+        y: canvasDom.height/2
     };
-    let canvasDom = document.querySelector('#ex4');
-    let ctx = canvasDom.getContext('2d');
-    ctx.fillStyle = circle.color;
-    ctx.beginPath();
-    ctx.arc(circle.x += 10, circle.y, circle.radius, 0, 2 * Math.PI);
+    
     let intervalId = setInterval(function() {
+        ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+        ctx.fillStyle = circle.color;
+        ctx.beginPath();
+        ctx.arc(circle.x+=10, circle.y, circle.radius, 0, 2 * Math.PI);
         ctx.fill();
-    }, 1500);
-    let clearInter = setInterval(function() {}, 1500);
-    clearInterval(clearInter);
-    ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+        if (circle.firstx +150 <= circle.x) {
+            clearInterval(intervalId);
+        }
+    }, 100);
 }
 
 function exercice5() {
+    
+    let canvasDom = document.querySelector('#ex5');
+    let ctx = canvasDom.getContext('2d');
+    let circle = {
+        color: "#0000FF",
+        radius: 10,
+        x: canvasDom.width/2,
+        y: canvasDom.height/2
+    };
 
+    let intervalId = setInterval(function() {
+        ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+        ctx.fillStyle = circle.color;
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circle.radius+=10, 0, 2 * Math.PI);
+        ctx.fill();
+        if (circle.radius >= canvasDom.height/2) {
+            clearInterval(intervalId);
+        }
+    }, 100);
 }
 
 function exercice6() {
